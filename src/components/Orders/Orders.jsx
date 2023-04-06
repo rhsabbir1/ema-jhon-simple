c818e0dbe41326660f8c5f62876920cde51a6e1a
rhsabbir1 <rhsabbir090@gmai.com>
2023-04-06 20:19:33 +0600

    implemented delete from cart

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Cart from '../Cart/Cart';
 import { useLoaderData } from 'react-router-dom';
 import ReviewItme from '../ReviewItem/ReviewItme';
 import './Order.css';
+import { removeFromDb } from '../../utilities/fakedb';
 
 const Orders = () => {
-    const cart = useLoaderData();
+    const saveCart = useLoaderData();
+    const [cart , setCart] = useState(saveCart)
+    const handleRemoveCart = (id)=>{
+        const remaning = cart.filter(product => product.id  !== id)
+        setCart(remaning)
+        removeFromDb(id)
+    }
     
     return (
         <div className='shop-container'>
@@ -14,6 +21,7 @@ const Orders = () => {
                 cart.map(product => <ReviewItme 
                     key={product.id}
                     product={product}
+                    handleRemoveCart={handleRemoveCart}
                 ></ReviewItme>)
               }
             </div>
--- src/components/ReviewItem/ReviewItme.jsx
+++ src/components/ReviewItem/ReviewItme.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import './ReviewItem.css'
 
-const ReviewItme = ({product}) => {
-    const {id , img , name , price , quantity} = product;
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+
+const ReviewItme = ({ product , handleRemoveCart }) => {
+    const { id, img, name, price, quantity } = product;
     return (
         <div className='review-item'>
-            <img src={img} alt="" />
+            <div className="review-details">
+                <img src={img} alt="" />
+                <div>
+                    <p>{name}</p>
+                    <p>Price : <span className='oranger-text'>$ {price}</span></p>
+                    <p>Quantity : <span className='oranger-text'> {quantity}</span></p>
+                </div>
+            </div>
+            <button onClick={()=> handleRemoveCart(id)} className='delete-btn'>
+                <FontAwesomeIcon className='delete-icon' icon={faTrashAlt} />
+            </button>
         </div>
     );
 };
